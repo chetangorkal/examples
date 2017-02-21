66db6d50128139de77d7daaaecf558d48921cc5d
Chetan Gorkal <chetankumar.1510@gmail.com>
2017-02-21 14:33:28 +0530

    filters

---
+++ servlets/src/me/example/filter/MyFilter.java
@@ -0,0 +1,49 @@
+package me.example.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * 
+ * @author Chetan Gorkal <br/>
+ *         Demo of {@link Filter}
+ */
+@WebFilter("/filter/*")
+public class MyFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public MyFilter() {
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		if (request.getParameter("name").equals("Chetan")) {
+			// pass the request along the filter chain
+			chain.doFilter(request, response);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
